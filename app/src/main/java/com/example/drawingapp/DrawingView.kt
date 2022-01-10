7815475637503b75b4eaa17ac9b161d5e4c54970
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-10 14:53:19 -0500

    Preparing the brush size selector

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionHelper
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+       // mBrushSize = 20.toFloat()
 
     }
 
@@ -97,6 +98,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     override fun invalidate() {
         super.invalidate()
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath (var color: Int, var brushThickness: Float) : Path() {
